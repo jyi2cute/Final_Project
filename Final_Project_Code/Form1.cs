e4859f44a5cc120a7b131860e2b22e89fa357161
Snoe <yurikorolev99@gmail.com>
2024-12-11 15:27:34 -0500

    login progress, storeddata file created

--- Final_Project_Code/Form1.cs
+++ Final_Project_Code/Form1.cs
@@ -1,7 +1,11 @@
+using static System.Formats.Asn1.AsnWriter;
+
 namespace Final_Project
 {
     public partial class Form1 : Form
     {
+        List<int> pastPurchases = new List<int> { };
+        List<int> cart = new List<int> { };  
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +28,7 @@ namespace Final_Project
             string password = passwordBox.Text;
 
             // Replace this with your actual authentication logic
+            login(username, password);
             if (username == "your_username" && password == "your_password")
             {
                 // Successful login
@@ -40,6 +45,40 @@ namespace Final_Project
         private void usernameBox_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private static void login(string username, string password)
+        {
+            bool found = false;
+            string filePath = "Data.txt";
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            var parts = line.Split(',');
+
+                            if (parts[0] == username && parts[1] == password)
+                            {
+                                found = true;
+                                Console.WriteLine($"Found login for {username}: {parts[1]}");
+                                pastPurchases = parts[2];
+                                cart = parts[3];
+                            }
+                        }
+                        if (!found) Console.WriteLine("No login found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading login: {ex.Message}");
+            }
         }
     }
 }
